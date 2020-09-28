1197c9b57cd664d9437426eb8f90c6f3934f89b3
NidhitheCoder <nidhpv@gmail.com>
2020-09-28 16:30:22 +0530

    Updates

---
+++ precache-manifest.0ac14ca9cc296e8709504116e0cdb3fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd78f40035334eea07672149fff6d5c",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "b3cae6f307e995af0e13",
+    "url": "/monster-app/static/css/main.10e32dd8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cae6f307e995af0e13",
+    "url": "/monster-app/static/js/main.a2f4a0a3.chunk.js"
+  },
+  {
+    "revision": "92a25797d6571872c4c3",
+    "url": "/monster-app/static/js/runtime-main.79c229cc.js"
+  }
+]);
